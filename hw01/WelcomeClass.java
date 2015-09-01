0c80efdefabde19ace8f59bca4f4a4cbef37460f
djd316 <chromequail@yahoo.com>
2015-09-01 22:04:13 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+//Daniel J. DePaul
+//hw01 
+
+//This program displays a welcome message followed by the 6 characters 
+//of my lehigh network ID, which is then followed by a personal message
+//define a class
+public class WelcomeClass{
+
+// add main method
+public static void main(String[] args) {
+
+// print the statement with the box around it
+  System.out.println("  ^  ^  ^  ^  ^  ^ "); //printing all these strings, have to use double backslashes or you get illegal character escape
+  System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+  System.out.println("|-d--j--d--3--1--6-|");
+  System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /  ");
+  System.out.println("  v  v  v  v  v  v  ");
+  System.out.println(" \"I will stay behind, to gaze at the Sun.");
+  System.out.println("  The Sun is a wondrous body, like a magnificient Father!");
+  System.out.println("  If only I could be so grossly incandescent!\"  --Soliare of Astora");
+  
+}
+}
---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+//Daniel DePaul
+//Lab01
+// Hellow World Java Program
+//
+// First compile the program javac HellowWorld.java
+//run the program java Helloworld//
+
+//define a class
+public class HelloWorld{
+
+// add main method
+public static void main(String[] args) {
+
+// print the statement with the box around it
+  System.out.println("############################");
+  System.out.println("############################");
+  System.out.println("                            ");
+  System.out.println("     Hello World 2015      #");
+  System.out.println("                            ");
+  System.out.println("############################");
+  System.out.println("############################");
+   System.out.println("test");
+   //test/testest
+}
+}
